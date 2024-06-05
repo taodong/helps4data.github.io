74d241cff75f08dfec27e18bf5c615f3e0315f0d
Tao Dong <taojdcn@gmail.com>
2024-06-04 22:49:48 -0700

    complete data provider section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 module.exports = {
   title: 'Learn Wedge Data Generator',
-//  themes: ['@docusaurus/theme-search-algolia'],
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
@@ -71,9 +70,13 @@ module.exports = {
             label: 'Tutorials',
           },
           {
-            type: 'html',
+            label: 'Wedge Data Generator',
             position: 'right',
-            value: '<a href="https://data.wedgeup.com" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-arrow-right-square-fill" viewBox="0 0 16 16"><path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1"/></svg>&nbsp;Wedge Data Generator</a>',
+            href: 'https://data.wedgeup.com'
+          },
+          {
+            type: 'search',
+            position: 'right'
           }
         ],
       },
@@ -81,6 +84,12 @@ module.exports = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      aloglia: {
+          appId: "OL3E6P4ZV2",
+          apiKey: "ade1a6583d188989018937539a1a7e22",
+          indexName: "taodongio",
+          contextualSearch: true
+      }
     })
 };
 
